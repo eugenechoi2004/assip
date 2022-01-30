904f66524891e3ffbb6679553611acbe0aeb3a6c
eugenechoi2004 <35019459+eugenechoi2004@users.noreply.github.com>
2022-01-30 17:36:47 -0500

    implementing sift

---
+++ imageSimilarity/compare.py
@@ -0,0 +1,23 @@
+import cv2
+img1 = cv2.imread("imageComparisons/CubbonPark1.jpeg")
+img2 = cv2.imread("imageComparisons/CubbonPark2.jpeg")
+
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+# sift
+sift = cv2.xfeatures2d.SIFT_create()
+
+keypoints_1, descriptors_1 = sift.detectAndCompute(img1, None)
+keypoints_2, descriptors_2 = sift.detectAndCompute(img2, None)
+
+bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+
+matches = bf.match(descriptors_1, descriptors_2)
+matches = sorted(matches, key=lambda x: x.distance)
+print(len(matches))
+img3 = cv2.drawMatches(img1, keypoints_1, img2,
+                       keypoints_2, matches[:50], img2, flags=2)
+
+# save cv2 image img3
+cv2.imwrite("imageComparisons/sift.jpeg", img3)
