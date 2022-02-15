b4ad7cca1f5c8541af95024a4d6dfd08ddbe7a24
eugenechoi2004 <35019459+eugenechoi2004@users.noreply.github.com>
2022-02-15 11:33:12 -0500

    added the imageID

--- mapillaryText/Associating.py
+++ mapillaryText/Associating.py
@@ -1,6 +1,27 @@
 # read in a csv file FinalLogo.csv
 '''
-divid the section up into blocks
+divide the section up into blocks
 And see which images fall into the blocks
 
 '''
+
+import pandas as pd
+
+logos = pd.read_csv(
+    '/Users/eugene/Documents/GitHub/assip/mapillaryText/FinalLogos.csv')
+
+# read in the outputBanks.txt file
+
+banks = open('outputBanks.txt', 'r').readlines()
+newFile = open('outputBanksOutput.txt', 'w')
+for bank in banks:
+    latb = float(bank.split(' ')[0])
+    longb = float(bank.split(' ')[1])
+    for i in range(len(logos['Lat'])):
+        lat = float(logos['Lat'][i])
+        long = float(logos['Long'][i])
+        if latb == lat and longb == long:
+            newFile.write(str(lat)+' '+str(long)+' ' +
+                          str(logos['ImageId'][i])+'\n')
+            break
+newFile.close()
